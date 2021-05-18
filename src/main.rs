6587c218cb4d9d361d84512e73fe95b9ef76d8fc
Arun Sunil <arunsunil019@gmail.com>
2021-05-17 22:56:48 -0700

    Enforcing that a single argument be provided

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ mod fibonacci;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() != 2 {
+        println!("Usage: ./rust-experiments-fibonacci [integer > 0]");
+        return;
+    }
+
     match args[1].parse::<u32>() {
       Ok(limit) => fibonacci::fibonacci_loop(limit + 1),
       Err(error) => panic!("ERROR: Unable to convert argument to integer: {}. \
