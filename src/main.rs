0d60a733ba7965250b8829743b0e84b48ff05ddb
Arun Sunil <arunsunil019@gmail.com>
2021-05-17 23:20:48 -0700

    Fixing formatting in main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,6 @@ fn main() {
     match args[1].parse::<u32>() {
       Ok(limit) => fibonacci::fibonacci_loop(limit + 1),
       Err(error) => panic!("ERROR: Unable to convert argument to integer: {}. \
-                                        \n\tUsage: ./rust-experiments-fibonacci [integer > 0]\n", error),
+                            \n\tUsage: ./rust-experiments-fibonacci [integer > 0]\n", error),
     };
 }
